5ac8b688ba851d4f8357237135d90ed78067a64e
mohamedzait20003 <21-101153@students.eui.edu.eg>
2026-04-01 00:51:09 -0400

    Updated the download data and the model training utilities for the dataset.

--- src/main/utils/__init__.py
+++ src/main/utils/__init__.py
@@ -1,10 +1,15 @@
+from .collator import Collator
+from .dataset import CustomDataset
 from .data_processor import Processor
 from .tts_synthesizer import Synthesizer
+
 
 
 # Utils Package Initialization
 
 _all_ = [
+    "Collator",
     "Processor",
-    "Synthesizer"
+    "Synthesizer",
+    "CustomDataset"
 ]
